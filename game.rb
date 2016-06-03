fd76288c3ec6e5b425d24547b0d46fcd3c589301
Jakub Żuchowski <ellmunzai@gmail.com>
2016-06-03 23:51:52 +0200

    very basic while loop

--- game.rb
+++ game.rb
@@ -1,3 +1,23 @@
-class Game
-	
+# przywitanie i przypisanie zgadywanego hasła do `input`
+
+print "\e[H\e[2J" # czyszczenie ekranu
+puts "Welcome to hangman!"
+puts " ----- "
+puts "Input phrase:"
+phrase = gets.strip # strip obcina enter na końcu getsa
+
+# pętla na zgadywanie hasła
+
+while true do
+  print "\e[H\e[2J" # czyszczenie ekranu pry każdym kroku pętli
+  input = gets.strip
+
+  case input
+  when "Q"  # input == "Q"
+    puts "quitting the game"
+    break # break wychodzi z ostatniej pętli – ostatnia pętla zaczeła się w linii 11
+  when phrase # input == phrase
+    puts "you won the game"
+    break
+  end
 end
